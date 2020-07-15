61ea2f6d2f918de5a9398f4192f4b9099379e109
vlad-donchenko <2pi14b.donchenko@gmail.com>
2020-07-16 00:01:38 +0300

    create site header mobile (WIP)

--- source/js/index.js
+++ source/js/index.js
@@ -1,1 +1,20 @@
-console.log(`create app`);
+const mainMenuElement = document.querySelector(`.nav`);
+const menuButtonElement = document.querySelector(`.nav__burger`);
+
+const menuItemWithDropdownElements = document.querySelectorAll(`.main-menu__item--drop`);
+
+
+menuButtonElement.addEventListener(`click`, () => {
+  menuButtonElement.classList.toggle(`burger--close`);
+  mainMenuElement.classList.toggle(`nav--open`);
+});
+
+[...menuItemWithDropdownElements].forEach((item) => {
+  item.addEventListener(`click`, (evt) => {
+    evt.stopPropagation();
+
+    if (evt.target.classList.contains('main-menu__item--drop')) {
+      item.classList.toggle(`main-menu__item--open`);
+    }
+  });
+});
