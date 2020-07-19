d28ae3f49f0dfffcb2d7ac5285f3c4428ecb4338
vlad-donchenko <2pi14b.donchenko@gmail.com>
2020-07-19 23:09:17 +0300

    create footer and reviews section

--- source/js/index.js
+++ source/js/index.js
@@ -133,7 +133,7 @@
     getMenuBehaviorByBreakpoint(currentDocumentWidth, currentDocumentHeight);
   });
 
-  $('#promo_slider').slick({
+  $(`#promo_slider`).slick({
     infinite: true,
     arrows: false,
     dots: true,
@@ -142,6 +142,28 @@
   });
 
 
-
+  $(`#testimonial_slider`).slick({
+    infinite: false,
+    dots: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    centerMode: true,
+    focusOnSelect : true,
+    centerPadding: '12%',
+    prevArrow: $('#testimonial_prev'),
+    nextArrow: $('#testimonial_next'),
+    mobileFirst: true,
+    rtl: false,
+    responsive: [
+      {
+        breakpoint: 599,
+        settings: {
+          infinite: true,
+          centerPadding: '39px',
+          slidesToShow: 2,
+        }
+      }
+    ]
+  });
 
 })();
