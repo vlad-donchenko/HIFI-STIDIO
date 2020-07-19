ac22f38d8df1abad0e4daeaaf1537ca4f913c8b7
vlad-donchenko <2pi14b.donchenko@gmail.com>
2020-07-19 16:55:03 +0300

    create trending items section

--- source/js/index.js
+++ source/js/index.js
@@ -1,139 +1,147 @@
-const BreakPoint = {
-  MOBILE_SMALL: 576,
-  MOBILE: 767,
-  TABLET: 1198,
-};
+"use strict";
 
-const styles = getComputedStyle(document.documentElement);
-const documentHeightValue = styles.getPropertyValue('--document-height');
+(function () {
+  const BreakPoint = {
+    MOBILE_SMALL: 576,
+    MOBILE: 767,
+    TABLET: 1198,
+  };
 
-const htmlElement = document.querySelector(`html`);
-const mainMenuElement = document.querySelector(`.nav`);
-const menuButtonElement = document.querySelector(`.nav__burger`);
-const menuItemWithDropdownElements = [...document.querySelectorAll(`.main-menu__item--drop`)];
-const mainSearchButtonElement = document.querySelector(`.user-info__link--search`);
+  const styles = getComputedStyle(document.documentElement);
+  const documentHeightValue = styles.getPropertyValue(`--document-height`);
 
-const userInfoListElement = document.querySelector(`.user-info`);
-const userOptionItemElement = userInfoListElement.querySelector(`.user-option`);
+  const htmlElement = document.querySelector(`html`);
+  const mainMenuElement = document.querySelector(`.nav`);
+  const menuButtonElement = document.querySelector(`.nav__burger`);
+  const menuItemWithDropdownElements = [...document.querySelectorAll(`.main-menu__item--drop`)];
+  const mainSearchButtonElement = document.querySelector(`.user-info__link--search`);
 
-let currentDocumentWidth = document.body.offsetWidth;
-let currentDocumentHeight = document.body.offsetHeight;
+  const userInfoListElement = document.querySelector(`.user-info`);
+  const userOptionItemElement = userInfoListElement.querySelector(`.user-option`);
 
-const toggleShowUseOptionListCLickHandle = () => {
-  userOptionItemElement.classList.toggle(`user-option--open`);
-};
+  let currentDocumentWidth = document.body.offsetWidth;
+  let currentDocumentHeight = document.body.offsetHeight;
 
-const toggleMainSearchClickHandle = () => {
-  if (mainMenuElement.classList.contains(`nav--open`)) {
-    mainMenuElement.classList.remove(`nav--open`);
-    menuButtonElement.classList.remove(`burger--close`);
-  }
+  const toggleShowUseOptionListCLickHandle = () => {
+    userOptionItemElement.classList.toggle(`user-option--open`);
+  };
 
-  mainMenuElement.classList.toggle(`nav--open-search`);
-};
+  const toggleMainSearchClickHandle = () => {
+    if (mainMenuElement.classList.contains(`nav--open`)) {
+      mainMenuElement.classList.remove(`nav--open`);
+      menuButtonElement.classList.remove(`burger--close`);
+    }
 
-mainSearchButtonElement.addEventListener(`click`, toggleMainSearchClickHandle);
+    mainMenuElement.classList.toggle(`nav--open-search`);
+  };
 
-const createMainMenuToggleElements = () => {
-  menuItemWithDropdownElements.forEach((item) => {
-    const toggle = document.createElement(`span`);
-    toggle.classList.add(`main-menu__item-toggle`);
-    item.querySelector(`.main-menu__link`).appendChild(toggle);
-  });
-};
+  mainSearchButtonElement.addEventListener(`click`, toggleMainSearchClickHandle);
 
-const removeMainMenuToggleElements = () => {
-  [...document.querySelectorAll(`.main-menu__item-toggle`)].forEach((toggle) => {
-    toggle.remove();
-  });
-};
-
-const toggleMainMenuClickHandler = () => {
-  if (mainMenuElement.classList.contains(`nav--open-search`)) {
-    mainMenuElement.classList.remove(`nav--open-search`);
-  }
-
-  htmlElement.classList.toggle(`open-nav`);
-  menuButtonElement.classList.toggle(`burger--close`);
-  mainMenuElement.classList.toggle(`nav--open`);
-};
-
-const getTabletMenuBehavior = () => {
-  menuItemWithDropdownElements.forEach((item) => {
-
-    const toggle = item.querySelector(`.main-menu__item-toggle`);
-
-    toggle.addEventListener(`click`, (evt) => {
-      evt.preventDefault();
-
-      const toggleParentElement = toggle.closest(`.main-menu__item`);
-
-      if (evt.target.classList.contains(`main-menu__item-toggle`)) {
-
-        menuItemWithDropdownElements.forEach((item) => {
-          if (toggleParentElement !== item) {
-            item.classList.remove(`main-menu__item--open`);
-          }
-        });
-
-        item.classList.toggle(`main-menu__item--open`);
-      }
+  const createMainMenuToggleElements = () => {
+    menuItemWithDropdownElements.forEach((item) => {
+      const toggle = document.createElement(`span`);
+      toggle.classList.add(`main-menu__item-toggle`);
+      item.querySelector(`.main-menu__link`).appendChild(toggle);
     });
-  });
-};
+  };
 
-const getMobileMenuBehavior = () => {
-  menuItemWithDropdownElements.forEach((item) => {
-
-    const toggle = item.querySelector(`.main-menu__item-toggle`);
-
-    toggle.addEventListener(`click`, (evt) => {
-      evt.preventDefault();
-
-      if (evt.target.classList.contains(`main-menu__item-toggle`)) {
-        item.classList.toggle(`main-menu__item--open`);
-      }
+  const removeMainMenuToggleElements = () => {
+    [...document.querySelectorAll(`.main-menu__item-toggle`)].forEach((toggle) => {
+      toggle.remove();
     });
-  });
-};
+  };
 
-const getMenuBehaviorByBreakpoint = (currentDocumentWidth, currentDocumentHeight) => {
- if (currentDocumentWidth > BreakPoint.MOBILE && currentDocumentWidth < BreakPoint.TABLET) {
-    removeMainMenuToggleElements();
-    createMainMenuToggleElements();
-    getTabletMenuBehavior();
-  } else if (currentDocumentWidth < BreakPoint.MOBILE) {
-   document.documentElement.style.setProperty(documentHeightValue, `${currentDocumentHeight}px`);
+  const toggleMainMenuClickHandler = () => {
+    if (mainMenuElement.classList.contains(`nav--open-search`)) {
+      mainMenuElement.classList.remove(`nav--open-search`);
+    }
 
-    removeMainMenuToggleElements();
-    createMainMenuToggleElements();
-    getMobileMenuBehavior();
-  } else if (currentDocumentWidth > BreakPoint.TABLET) {
-    mainMenuElement.classList.remove(`nav--open`);
-    menuButtonElement.classList.remove(`burger--close`);
-    removeMainMenuToggleElements();
-  }
-};
+    htmlElement.classList.toggle(`open-nav`);
+    menuButtonElement.classList.toggle(`burger--close`);
+    mainMenuElement.classList.toggle(`nav--open`);
+  };
 
-createMainMenuToggleElements();
+  const getTabletMenuBehavior = () => {
+    menuItemWithDropdownElements.forEach((item) => {
 
-getMenuBehaviorByBreakpoint(currentDocumentWidth, currentDocumentHeight);
+      const toggle = item.querySelector(`.main-menu__item-toggle`);
 
-menuButtonElement.addEventListener(`click`, toggleMainMenuClickHandler);
+      toggle.addEventListener(`click`, (evt) => {
+        evt.preventDefault();
 
-userOptionItemElement.addEventListener(`click`, toggleShowUseOptionListCLickHandle);
+        const toggleParentElement = toggle.closest(`.main-menu__item`);
 
-window.addEventListener(`resize`, () => {
-  currentDocumentWidth = document.body.offsetWidth;
-  currentDocumentHeight = document.body.offsetHeight;
+        if (evt.target.classList.contains(`main-menu__item-toggle`)) {
+
+          menuItemWithDropdownElements.forEach((item) => {
+            if (toggleParentElement !== item) {
+              item.classList.remove(`main-menu__item--open`);
+            }
+          });
+
+          item.classList.toggle(`main-menu__item--open`);
+        }
+      });
+    });
+  };
+
+  const getMobileMenuBehavior = () => {
+    menuItemWithDropdownElements.forEach((item) => {
+
+      const toggle = item.querySelector(`.main-menu__item-toggle`);
+
+      toggle.addEventListener(`click`, (evt) => {
+        evt.preventDefault();
+
+        if (evt.target.classList.contains(`main-menu__item-toggle`)) {
+          item.classList.toggle(`main-menu__item--open`);
+        }
+      });
+    });
+  };
+
+  const getMenuBehaviorByBreakpoint = (currentDocumentWidth, currentDocumentHeight) => {
+    if (currentDocumentWidth > BreakPoint.MOBILE && currentDocumentWidth < BreakPoint.TABLET) {
+      removeMainMenuToggleElements();
+      createMainMenuToggleElements();
+      getTabletMenuBehavior();
+    } else if (currentDocumentWidth < BreakPoint.MOBILE) {
+      document.documentElement.style.setProperty(documentHeightValue, `${currentDocumentHeight}px`);
+
+      removeMainMenuToggleElements();
+      createMainMenuToggleElements();
+      getMobileMenuBehavior();
+    } else if (currentDocumentWidth > BreakPoint.TABLET) {
+      mainMenuElement.classList.remove(`nav--open`);
+      menuButtonElement.classList.remove(`burger--close`);
+      removeMainMenuToggleElements();
+    }
+  };
+
+  createMainMenuToggleElements();
 
   getMenuBehaviorByBreakpoint(currentDocumentWidth, currentDocumentHeight);
-});
 
-$('#promo_slider').slick({
-  infinite: true,
-  arrows: false,
-  dots: true,
-  slidesToShow: 1,
-  slidesToScroll: 1
-});
+  menuButtonElement.addEventListener(`click`, toggleMainMenuClickHandler);
+
+  userOptionItemElement.addEventListener(`click`, toggleShowUseOptionListCLickHandle);
+
+  window.addEventListener(`resize`, () => {
+    currentDocumentWidth = document.body.offsetWidth;
+    currentDocumentHeight = document.body.offsetHeight;
+
+    getMenuBehaviorByBreakpoint(currentDocumentWidth, currentDocumentHeight);
+  });
+
+  $('#promo_slider').slick({
+    infinite: true,
+    arrows: false,
+    dots: true,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  });
+
+
+
+
+})();
