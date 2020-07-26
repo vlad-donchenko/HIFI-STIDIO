84b4eafef7e2e371cd9ff610d85cb7188ab0c8db
vlad-donchenko <2pi14b.donchenko@gmail.com>
2020-07-26 18:48:04 +0300

    create sing in

--- source/js/site-footer.js
+++ source/js/site-footer.js
@@ -2,18 +2,22 @@
 
 (function () {
   const footer = document.querySelector('.site-footer');
-  const footerMenuToggles = [...footer.querySelectorAll('.menu-footer__toggle')];
+
+  if (footer) {
+    const footerMenuToggles = [...footer.querySelectorAll('.menu-footer__toggle')];
 
 
-  const toggleShowSubMenuClickHandler = (evt) => {
-    evt.preventDefault();
+    const toggleShowSubMenuClickHandler = (evt) => {
+      evt.preventDefault();
 
-    const button = evt.target;
+      const button = evt.target;
 
-    button.closest('.menu-footer__column').classList.toggle('menu-footer__column--open');
-  };
+      button.closest('.menu-footer__column').classList.toggle('menu-footer__column--open');
+    };
 
-  footerMenuToggles.forEach((toggle) => {
-    toggle.addEventListener('click', toggleShowSubMenuClickHandler);
-  });
+    footerMenuToggles.forEach((toggle) => {
+      toggle.addEventListener('click', toggleShowSubMenuClickHandler);
+    });
+  }
+
 })();
