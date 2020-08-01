85cfeca0f97f0ea20c9ef653f6c7869c4edf2c56
vlad-donchenko <2pi14b.donchenko@gmail.com>
2020-08-01 16:54:05 +0300

    create coming soon page

--- source/js/index.js
+++ source/js/index.js
@@ -103,23 +103,24 @@
   }
 
   const brandSearchButton = document.querySelector('.brands__item--search');
-  const searchBrandSection = document.querySelector('.search-brand');
-  const closeSearchByBrandButton = searchBrandSection.querySelector('.search-brand__close');
-
-  const openSearchByBrandClickHandler = (evt) => {
-    evt.preventDefault();
-    window.header.applyViewPort();
-    window.header.root.classList.add('overflow-hidden');
-    searchBrandSection.classList.add('search-brand--open');
-  };
-
-  const closeSearchByBrandClickHandler = () => {
-    window.header.applyViewPort();
-    window.header.root.classList.remove('overflow-hidden');
-    searchBrandSection.classList.remove('search-brand--open');
-  };
 
   if (brandSearchButton) {
+    const searchBrandSection = document.querySelector('.search-brand');
+    const closeSearchByBrandButton = searchBrandSection.querySelector('.search-brand__close');
+
+    const openSearchByBrandClickHandler = (evt) => {
+      evt.preventDefault();
+      window.header.applyViewPort();
+      window.header.root.classList.add('overflow-hidden');
+      searchBrandSection.classList.add('search-brand--open');
+    };
+
+    const closeSearchByBrandClickHandler = () => {
+      window.header.applyViewPort();
+      window.header.root.classList.remove('overflow-hidden');
+      searchBrandSection.classList.remove('search-brand--open');
+    };
+
     closeSearchByBrandButton.addEventListener('click', closeSearchByBrandClickHandler);
     brandSearchButton.addEventListener('click', openSearchByBrandClickHandler);
   }
